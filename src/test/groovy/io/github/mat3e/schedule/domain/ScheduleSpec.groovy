13f8bc20860516c8bc1d5fbbb0798660e06c16d8
Mateusz Chrzonstowski <mateusz@brightcube.eu>
2020-03-06 23:47:45 +0100

    Add Spock and change from Maven to Gradle

---
+++ src/test/groovy/io/github/mat3e/schedule/domain/ScheduleSpec.groovy
@@ -0,0 +1,45 @@
+package io.github.mat3e.schedule.domain
+
+import spock.lang.Specification
+
+class ScheduleSpec extends Specification {
+    def 'should throw when scheduling for the taken date'() {
+        expect:
+        true
+    }
+
+    def 'should throw when scheduling with the taken room'() {
+        expect:
+        true
+    }
+
+    def 'should schedule a new on call date'() {
+        expect:
+        true
+    }
+
+    def 'should throw when scheduling a visit to an absent doctor'() {
+        expect:
+        true
+    }
+
+    def 'should throw when scheduling a visit for the taken date'() {
+        expect:
+        true
+    }
+
+    def 'should schedule a new visit'() {
+        expect:
+        true
+    }
+
+    def 'should throw when overriding on call with a doctor with a different specialization'() {
+        expect:
+        true
+    }
+
+    def 'should override on call'() {
+        expect:
+        true
+    }
+}
